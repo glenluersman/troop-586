935e6c0750cd82e3463997660d52574962efa4ca
glenluersman <glen.luersman@gmail.com>
2022-05-01 12:58:48 -0400

    back to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+mybutton = document.getElementById("myBtn");
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "400px";
   document.getElementById("main").style.marginLeft = "430px";
@@ -8,4 +10,20 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "30px";
   document.body.style.backgroundColor = "white";
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
